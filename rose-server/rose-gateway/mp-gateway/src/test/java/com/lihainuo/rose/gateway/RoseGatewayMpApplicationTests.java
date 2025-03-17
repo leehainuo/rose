90d88c9c87ccd49df0fb34c51f214a748f80b314
lihainuo <lihainuo0502@gmail.com>
2025-03-17 23:57:06 +0800

    refactor:项目目录重构

---
+++ rose-server/rose-gateway/mp-gateway/src/test/java/com/lihainuo/rose/gateway/RoseGatewayMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihainuo.rose.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoseGatewayMpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
