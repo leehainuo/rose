02e5db8475e07706054b4900b4fbe872f4534b5d
lihainuo <lihainuo0502@gmail.com>
2025-03-17 23:22:50 +0800

    init:项目初始化完成

---
+++ rose-server/rose-common/src/test/java/com/lihainuo/rose/common/RoseCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lihainuo.rose.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoseCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
